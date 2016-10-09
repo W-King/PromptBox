712b1fc3ba1dfee17118d97052e3ed614d63f5cb
jiboxue <jibx@ichsy.com>
2016-10-09 17:54:35 +0800

    first commit

---
+++ PPX_PromptLabel/PromptLabel/PromptView.h
@@ -0,0 +1,13 @@
+//
+//  PromptView.h
+//  PPX_PromptLabel
+//
+//  Created by pipixia on 16/10/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PromptView : UIView
+
+@end
---
+++ PPX_PromptLabel/PromptLabel/PromptView.m
@@ -0,0 +1,21 @@
+//
+//  PromptView.m
+//  PPX_PromptLabel
+//
+//  Created by pipixia on 16/10/9.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import "PromptView.h"
+
+@implementation PromptView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
